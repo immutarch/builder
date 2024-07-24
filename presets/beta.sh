ef8064b6f09ec22236e194bb9b5af6a4e561b61a
Adam Jafarov <thevakhovske@petalmail.com>
2024-07-24 19:52:19 +0300

    broken

--- presets/beta.sh
+++ presets/beta.sh
@@ -9,11 +9,11 @@ echo -e ${RELEASETAG} > /tmp/builder-releasetag
 echo -e "$(echo ${DISTRO_NAME} | tr '[:upper:]' '[:lower:]')_$(date +%Y%m%d%H%M)_$(echo ${OS_CODENAME} | tr '[:upper:]' '[:lower:]')_${RELEASETAG}" > /tmp/build_temp_ver
 export FLAVOR_BUILDVER=$(cat /tmp/build_temp_ver)
 export IMAGEFILE="${FLAVOR_BUILDVER}"
-export FLAVOR_CHROOT_SCRIPTS="sddm bluetooth sshd systemd-timesyncd NetworkManager ima-offload.target var-lib-pacman.mount nix.mount opt.mount root.mount srv.mount usr-lib-debug.mount usr-local.mount var-cache-pacman.mount var-lib-docker.mount var-lib-flatpak.mount var-lib-systemd-coredump.mount var-log.mount var-tmp.mount"
+export FLAVOR_CHROOT_SCRIPTS="sddm bluetooth sshd nix-daemon systemd-timesyncd NetworkManager ima-offload.target var-lib-pacman.mount nix.mount opt.mount root.mount srv.mount usr-lib-debug.mount usr-local.mount var-cache-pacman.mount var-lib-docker.mount var-lib-flatpak.mount var-lib-systemd-coredump.mount var-log.mount var-tmp.mount"
 #export FLAVOR_PLYMOUTH_THEME="steamos"
 export FLAVOR_FINAL_DISTRIB_IMAGE=$FLAVOR_BUILDVER
 export KERNELCHOICE="linux-lts"
-export BASE_BOOTSTRAP_PKGS="base base-devel nix-daemon flatpak arch-install-scripts linux-firmware amd-ucode intel-ucode sddm dkms jq btrfs-progs grub efibootmgr openssh"
+export BASE_BOOTSTRAP_PKGS="base base-devel flatpak arch-install-scripts linux-firmware amd-ucode intel-ucode sddm dkms jq btrfs-progs grub efibootmgr openssh"
 export UI_BOOTSTRAP="plasma plasma-nm dolphin konsole kate sddm mesa vulkan-radeon vulkan-intel nix spectacle gwenview"
 export IMA_RELEASE="IMAGE_ID=\"${FLAVOR_BUILDVER}\"\nOS_TAG=${RELEASETAG}\nRELEASETYPE=$(echo ${OS_CODENAME} | tr '[:upper:]' '[:lower:]')\nISINTERNAL=no"
 export PACMAN_ONLOAD="[Unit]\nDescription=${DISTRO_NAME} onload - /var/lib/pacman\n\n[Mount]\nWhat=/${OS_FS_PREFIX}_root/rootfs/${FLAVOR_FINAL_DISTRIB_IMAGE}/var/lib/pacman\nWhere=/var/lib/pacman\nType=none\nOptions=bind\n\n[Install]\nWantedBy=ima-offload.target"
